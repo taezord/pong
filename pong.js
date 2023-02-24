e4e6846d363ea16d1bddc5f0c6cc29b26f478c53
Taiane Soler <87228793+taezord@users.noreply.github.com>
2023-02-24 11:34:06 -0300

    refatoração do código

--- pong.js
+++ pong.js
@@ -1,9 +1,10 @@
-
+//variáveis da bolinha
 let xBall = 300;
 let yBall = 200;
 let diametro = 18;
 let raio = diametro /2;
 
+//velocidade da bolinha
 let velocidadeXBall = 6;
 let velocidadeYBall = 6;
 
@@ -16,19 +17,37 @@ function setup() {
 
 function draw() {
   background(0);
-  circle(xBall, yBall, diametro);
-  xBall += velocidadeXBall;
-  yBall += velocidadeYBall;
+  mostraBolinha();
+  movimentaBolinha();
+  verificaColisaoBorda();
   
-  if (xBall + raio > width || xBall - raio < 0) {
+  
+  function mostraBolinha() {
+    
+    circle(xBall, yBall, diametro);
+    
+  }
+  
+  function movimentaBolinha() {
+    
+    xBall += velocidadeXBall;
+    yBall += velocidadeYBall;
+    
+  }
+  
+  function verificaColisaoBorda() {
+    
+    if (xBall + raio > width || xBall - raio < 0) {
     
     velocidadeXBall *= -1;
     
+    }
+    
+    if(yBall + raio > height || yBall - raio < 0){
+      velocidadeYBall *= -1;
+    }
+    
+    
   }
-  if(yBall + raio > height || yBall - raio < 0){
-    velocidadeYBall *= -1;
-  }
-  
-  
   
 }
