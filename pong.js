2877ddd1f2e783537369f7f5862f47fed801006e
Taiane Soler <87228793+taezord@users.noreply.github.com>
2023-02-24 11:13:04 -0300

    ajuste da movimentação e colisão do ator

--- pong.js
+++ pong.js
@@ -1,7 +1,11 @@
+
 let xBall = 300;
 let yBall = 200;
 let diametro = 18;
-let velocidadeXBall = 5;
+let raio = diametro /2;
+
+let velocidadeXBall = 6;
+let velocidadeYBall = 6;
 
 
 
@@ -9,8 +13,22 @@ function setup() {
   createCanvas(600, 400);
 }
 
+
 function draw() {
   background(0);
   circle(xBall, yBall, diametro);
   xBall += velocidadeXBall;
+  yBall += velocidadeYBall;
+  
+  if (xBall + raio > width || xBall - raio < 0) {
+    
+    velocidadeXBall *= -1;
+    
+  }
+  if(yBall + raio > height || yBall - raio < 0){
+    velocidadeYBall *= -1;
+  }
+  
+  
+  
 }
